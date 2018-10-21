7411b34ed2b0fd5545a2b6614386103d7e36bae0
Katarzyna Chachlowska <katarzyna.chachlowska@netguru.pl>
2018-10-21 12:07:15 +0200

    Add templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index') #decorators
 def index():
-    return "Hello, world!"
+    user = {"username": "Kasia"}
+    posts = [
+        {
+            'author': {"username": 'Kath'},
+            'body': "It's a beautiful day"
+        },
+        {
+            'author': {"username": 'Marco'},
+            'body': "I like trains"
+        }
+    ]
+    return render_template('index.html', title="Kcht's blog", user=user, posts = posts)
